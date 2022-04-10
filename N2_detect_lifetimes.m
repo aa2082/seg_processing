de6ef1700e1ddd2ff60ea88a9cf00b544c2009c4
aa2082 <aa2082@cam.ac.uk>
2022-04-10 20:09:02 +0100

    processing seems to work, finishing analysis in progress

--- N2_detect_lifetimes.m
+++ N2_detect_lifetimes.m
@@ -3,7 +3,7 @@ plot_lt = false;
 dir_save = '/Users/ali/Desktop/lt_graphs/';
 dir_workspace = "/Users/ali/seg_processing/";
 n_m = size(data,1);
-x = 1:721;
+x_t = 1:721;
 res = 0.01;
 x_sp = 1:res:721;
 % When calculating the growth rate, linear fitting is used on the observed
@@ -14,43 +14,57 @@ x_sp = 1:res:721;
 n_l_trim_gr_fit = 1;
 n_r_trim_gr_fit = 1; 
 
-%div_t = [];
-div_t = {};
-c = [];
 lt_data = {};
-lt_results = {};
+lt_results = struct;
+lt_results.mother_cell_number = [];
+lt_results.lifetime_number = [];
+lt_results.division_time = [];
+lt_results.rfp_intensity = [];
+lt_results.yfp_intensity = [];
+lt_results.number_of_observations = [];
+lt_results.r2 = [];
+lt_results.poly_fit = [];
+lt_results.growth_rate = [];
 for cell_num=1:n_m
     cell = log(data(cell_num,:,2));
-    sp = spline(x,cell,x_sp);
+    sp = spline(x_t,cell,x_sp);
     der = -diff(sp)./res; %derivative of log area interpolation
-    [pk,loc] = findpeaks(der,'MinPeakHeight',0.4,'MinPeakDistance',5); %finding intervals from peaks in der
-%     
-%     dt = diff(loc).*res;
-%     %div_t=[div_t,dt]; %div times are differences between interval seperators 
-%     div_t{end+1} = dt;
-%     mot = dt(1:end-1);
-%     dau = dt(2:end);
-%     c = [c;mean((mot-mean(mot)).*(dau-mean(dau)))./(std(mot)*std(dau))];
+    [pk,loc] = findpeaks(der,'MinPeakHeight',0.4,'MinPeakDistance',5/res); %finding intervals from peaks in der
     
     lt_window = [x_sp(loc(1:end-1)).',x_sp(loc(2:end)).'];
-    for a = 1:length(lt_window)
-        %"cell# "+cell_num + " lt# " + a
+    for a = 1:size(lt_window,1)
+        lt_results.mother_cell_number(end+1,1)= cell_num;
+        lt_results.lifetime_number(end+1,1)= a;
+
         c_data = squeeze(data(cell_num,:,:));
-        lt_data{cell_num,a} = c_data((lt_window(a,1)<c_data(:,28) & lt_window(a,2)>c_data(:,28)),:);
-        lt_results{cell_num,a}.divt = lt_data{cell_num,a}(end,28)-lt_data{cell_num,a}(1,28);
+        lt_data{end+1,1} = c_data((lt_window(a,1)<c_data(:,28) & lt_window(a,2)>c_data(:,28)),:);
+        lt_results.division_time(end+1,1) = lt_data{end,1}(end,28)-lt_data{end,1}(1,28);
         
+        lt_results.rfp_intensity(end+1,1) = nanmean(lt_data{end,1}(:,23));
+        lt_results.yfp_intensity(end+1,1)= nanmean(lt_data{end,1}(:,36));
+
+        lt_results.number_of_observations(end+1,1) = size(lt_data{end,1},1);
         %range of observations used for growth rate calculation
-        range_for_gr = n_l_trim_gr_fit+1:size(lt_data{cell_num,a},1)-n_r_trim_gr_fit;
+        range_for_gr = n_l_trim_gr_fit+1:size(lt_data{end,1},1)-n_r_trim_gr_fit;
         if(length(range_for_gr)>1) %need more than one point of interest for linear interpolation
-            lt_results{cell_num,a}.p_fit = polyfit(lt_data{cell_num,a}(range_for_gr,28),log(lt_data{cell_num,a}(range_for_gr,2)),1);
-            lt_results{cell_num,a}.gr = lt_results{cell_num,a}.p_fit(1,1);
-    
-            SS_res = sum(abs(log(lt_data{cell_num,a}(range_for_gr,2))-polyval(lt_results{cell_num,a}.p_fit,lt_data{cell_num,a}(range_for_gr,28))));
-            SS_tot = var(log(lt_data{cell_num,a}(range_for_gr,2)))*length(range_for_gr);
-            lt_results{cell_num,a}.r2 = 1-(SS_res/SS_tot);
+            x = lt_data{end,1}(range_for_gr,28);
+            y = log(lt_data{end,1}(range_for_gr,2));
+
+            p = polyfit(x,y,1);
+            yfit = polyval(p,x);
             
-            lt_results{cell_num,a}.rfp_intden = nanmean(lt_data{cell_num,a}(:,23));
-            lt_results{cell_num,a}.yfp_intden = nanmean(lt_data{cell_num,a}(:,36));
+            SStot = sum((y-mean(y)).^2);                  
+            SSres = sum((y-yfit).^2);      
+            Rsq = 1-SSres/SStot;
+        
+            lt_results.poly_fit(end+1,:) = p;
+            lt_results.growth_rate(end+1,1) = p(1,1);
+            lt_results.r2(end+1,1) = Rsq;
+        else
+            lt_results.poly_fit(end+1,1) = nan;
+            lt_results.growth_rate(end+1,1) = nan;
+            lt_results.r2(end+1,1) = nan;
+
         end
         if(plot_lt==true)
             run(dir_workspace+"single_lt_graph.m");
