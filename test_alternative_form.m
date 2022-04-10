0e01edb14494c6d56bdac8dc1be9fe3492eef139
aa2082 <aa2082@cam.ac.uk>
2022-04-10 20:45:46 +0100

    cleaning up

--- test_alternative_form.m
+++
@@ -1,8 +0,0 @@
-gr = nan(size(lt_results));
-for i=1:size(lt_results,1)
-    for j=1:size(lt_results,2)
-       if(isa(lt_results{i,j},'struct'))
-            gr(i,j) = lt_results{i,j}.gr;
-       end
-    end
-end
