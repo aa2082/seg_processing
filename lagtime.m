76bf605036407485d9f1ef391386b7c34f92b931
aa2082 <aa2082@cam.ac.uk>
2022-05-21 18:00:39 +0100

    finishing first draft - but need to add simulaiton of effects of noise on population growth rates

--- lagtime.m
+++ lagtime.m
@@ -1,15 +1,16 @@
-function [lagtime,lagtime_asym,csum,cv] = lagtime(max_gr,log_area,range_fit)
+function [lagt] = lagtime(max_gr,log_area)
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
     div_threshold = 0.2;
     gr = diff(log_area);
     gr(abs(gr)>div_threshold)=0;
     csum = cumsum(gr);
-    lagtime = (0:719) - cumsum(gr)/max_gr;
-    g
-    lagt_in_range = lagtime(1,range_fit);
+    lagt = (0:719) - cumsum(gr)/max_gr;
 
-    lagtime_asym = polyfit(range_fit,lagt_in_range,0);
 
-    cv = std(lagt_in_range-lagtime_asym)/mean(lagt_in_range-lagtime_asym);
+%     smooth_lagt =  smoothdata(lagt,'gaussian');
+%     findpeaks(diff(smooth_lagt),"SortStr",'descend','MinPeakDistance',100)
+%     [pks,locs] = findpeaks(diff(smooth_lagt),"SortStr",'descend','MinPeakDistance',10);
+%     loc_exp = locs(1);
+%     lag_to_exp = lagt(loc_exp);
 end
